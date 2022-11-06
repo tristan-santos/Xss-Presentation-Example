62674949eace13e59939bdf782d2714683ae5cdc
Tristan Santos <105960584+tristan-santos@users.noreply.github.com>
2022-11-06 18:13:44 +0800

    Delete public directory

--- public/js/script.mjs
+++
@@ -1,9 +0,0 @@
-export function convert(str) {
-	return new Promise((resolve, reject) => {
-		if (str) {
-			resolve('There was a String ', str)
-		} else {
-			reject('There was no string')
-		}
-	})
-}
